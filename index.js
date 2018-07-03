d0f3f659326abc723758adf33fb274715f43ce6f
bax-sp <746780@gmail.com>
2018-07-03 08:29:09 -0300

    Banco criado!

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000; //porta padrão
+const mysql = require('mysql');
+
+//configurando o body parser para pegar POSTS mais tarde
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//definindo as rotas
+const router = express.Router();
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+app.use('/', router);
+
+//inicia o servidor
+app.listen(port);
+console.log('API funcionando!');
